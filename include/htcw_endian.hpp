f59cb7807d766eae7fcce38e131d4cce4aac6f3c
codewitch-honey-crisis <gaztoast@gmail.com>
2023-10-04 03:14:17 -0700

    yet another fix in swap 64 bit

--- include/htcw_endian.hpp
+++ include/htcw_endian.hpp
@@ -73,7 +73,7 @@ constexpr static inline uint64_t swap(uint64_t value) {
 #if defined(__llvm__) || (defined(__GNUC__) && !defined(__ICC))
     return __builtin_bswap64(value);
 #else
-    uint64_t x = (uint64_t)x;
+    uint64_t x = (uint64_t)value;
     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
     x = (x & 0x00FF00FF00FF00FF) << 8 | (x & 0xFF00FF00FF00FF00) >> 8;
